d58886827b2aa78fb36d6d3e4cd1af79caaaa4c6
Michele Orrù <maker@tumbolandia.net>
2016-05-13 08:47:10 +0200

    Get rid of Yojson in acme_client.ml.

--- src/acme_client.ml
+++ src/acme_client.ml
@@ -4,7 +4,6 @@ open Lwt
 
 open Acme_common
 
-module J = Yojson.Basic
 module Pem = X509.Encoding.Pem
 
 type client_t = {
@@ -24,7 +23,7 @@ let return_ok a = return (Ok a)
 let return_error e = return (Error e)
 
 let malformed_json j =
-  let msg = Printf.sprintf "malformed json: %s" (J.to_string j) in
+  let msg = Printf.sprintf "malformed json: %s" (Json.to_string j) in
   Error msg
 
 let error_in endpoint code body =
@@ -32,7 +31,6 @@ let error_in endpoint code body =
   let errf = Printf.sprintf "Error at %s: code %d - body: %s" in
   let msg = errf endpoint code body in
   return_error msg
-
 
 (** I guess for now we can leave a function here of type
  * bytes -> bytes -> unit
@@ -66,31 +64,38 @@ let extract_nonce =
   get_header_or_fail "Replay-Nonce"
 
 let discover directory_url =
-  let directory_url = Uri.of_string directory_url in
-  http_get directory_url >>= fun (code, headers, body) ->
+  let directory = Uri.of_string directory_url in
+  http_get directory >>= fun (code, headers, body) ->
   extract_nonce headers  >>= fun nonce ->
-  let directory =
-    let dir = J.from_string body in
-    let member m = J.Util.member m dir |> J.Util.to_string |> Uri.of_string in
-    {
-      directory = directory_url;
-      new_authz = member "new-authz";
-      new_reg = member "new-reg";
-      new_cert = member "new-cert";
-      revoke_cert = member "revoke-cert";
-    }
-  in
-  return (nonce, directory)
+  match Json.of_string body with
+  | None -> error_in "discovery" code body
+  | Some edir ->
+     let p m = Json.string_member m edir in
+     match p "new-authz", p "new-reg", p "new-cert", p "revoke-cert" with
+     | Some new_authz, Some new_reg, Some new_cert, Some revoke_cert ->
+        let u = Uri.of_string in
+        let directory_t = {
+            directory = directory;
+            new_authz = u new_authz;
+            new_reg = u new_reg;
+            new_cert = u new_cert;
+            revoke_cert = u revoke_cert} in
+        return_ok (nonce, directory_t)
+     | _, _, _, _ ->
+        return (malformed_json edir)
 
 let new_cli directory_url rsa_pem csr_pem =
   let maybe_rsa = Primitives.priv_of_pem rsa_pem in
   let maybe_csr = Pem.Certificate_signing_request.of_pem_cstruct (Cstruct.of_string csr_pem) in
   match maybe_rsa, maybe_csr with
+  | None, _ -> return_error "Error parsing account key."
+  | Some x, [] -> return_error "Error parsing signing request."
+  | Some x, y0 :: y1 :: ys -> return_error "Error: too many signing requests."
   | Some account_key, [csr] ->
-     discover directory_url >>= fun (next_nonce, d)  ->
-     return_ok {account_key; csr; next_nonce; d}
-  | _ ->
-     return_error "Error: there's a problem paring those pem files."
+       discover directory_url >>= function
+       | Error e -> return_error e
+       | Ok (next_nonce, d)  ->
+          return_ok {account_key; csr; next_nonce; d}
 
 let cli_recv = http_get
 
@@ -122,21 +127,18 @@ let new_reg cli =
   | _   -> error_in "new-reg" code body
 
 let get_http01_challenge authorization =
-  let challenges_list = J.Util.member "challenges" authorization in
-  match challenges_list with
-  | `List challenges ->
-     begin
-       let is_http01 c = J.Util.member "type" c = `String "http-01" in
-       match List.filter is_http01 challenges with
-       | []  -> Error "No supported challenges found."
-       | challenge :: _ ->
-          let token = J.Util.member "token" challenge in
-          let url = J.Util.member "uri" challenge in
-          match token, url with
-          | `String t, `String u -> Ok {token=t; url=Uri.of_string u}
-          | _ -> malformed_json authorization
-     end
-  | _ -> malformed_json authorization
+  match Json.list_member "challenges" authorization with
+  | None -> malformed_json authorization
+  | Some challenges ->
+     let is_http01 c = Json.string_member "type" c = Some"http-01" in
+     match List.filter is_http01 challenges with
+     | []  -> Error "No supported challenges found."
+     | challenge :: _ ->
+        let token = Json.string_member "token" challenge in
+        let url = Json.string_member "uri" challenge in
+        match token, url with
+        | Some t, Some u -> Ok {token=t; url=Uri.of_string u}
+        | _, _ -> malformed_json authorization
 
 let do_http01_challenge cli challenge acme_dir =
   let token = challenge.token in
@@ -155,8 +157,11 @@ let new_authz cli domain =
   cli_send cli body url >>= fun (code, headers, body) ->
   match code with
   | 201 ->
-     let authorization = J.from_string body in
-     return (get_http01_challenge authorization)
+     begin
+       match Json.of_string body with
+       | Some authorization -> return (get_http01_challenge authorization)
+       | None -> error_in "new-auth" code body
+     end
   (* XXX. any other codes to handle? *)
   | _ -> error_in "new-authz" code body
 
--- src/json.mli
+++ src/json.mli
@@ -4,6 +4,7 @@ val of_string : string -> t option
 val to_string : ?comma:string -> ?colon:string -> t -> string
 
 val string_member : string -> t -> string option
+val list_member : string -> t -> t list option
 val b64_string_member : string -> t -> string option
 val b64_z_member : string -> t -> Z.t option
 val json_member : string -> t -> t option
