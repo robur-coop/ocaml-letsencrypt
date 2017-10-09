dee3491059359ab8cb29f70b25d9312db4f987d3
Hannes Mehnert <hannes@mehnert.org>
2017-10-10 00:47:19 +0100

    Mostly whitespace changes.

--- src/b64u.ml
+++ src/b64u.ml
@@ -1,29 +1,25 @@
 (** string head *)
-let head s =
-  s.[0]
+let head s = s.[0]
 
 (** string tail *)
-let tail s =
-  String.sub s 1 (String.length s - 1)
+let tail s = String.sub s 1 (String.length s - 1)
 
 let rec trim_leading_null s =
-  if head s == Char.chr 0 then trim_leading_null (tail s)
-  else s
+  if head s == Char.chr 0 then trim_leading_null (tail s) else s
 
 (** byte reversing *)
 let rev s =
   let rec revs strin list index =
-    if List.length list = String.length strin
-    then String.concat "" list
-    else revs strin ((String.sub strin index 1)::list) (index+1)
+    if List.length list = String.length strin then
+      String.concat "" list
+    else
+      revs strin (String.sub strin index 1 :: list) (index + 1)
   in
   revs s [] 0
 
-let urlencode =
-  B64.encode ~pad:false ~alphabet:B64.uri_safe_alphabet
+let urlencode = B64.encode ~pad:false ~alphabet:B64.uri_safe_alphabet
 
-let urldecode =
-  B64.decode ~alphabet:B64.uri_safe_alphabet
+let urldecode = B64.decode ~alphabet:B64.uri_safe_alphabet
 
 let urlencodez z =
   let bits = Z.to_bits z in
