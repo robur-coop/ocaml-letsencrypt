5fce847ebb4ff366990675e149ab8189a598025a
Ulrik Strid <ulrik.strid@outlook.com>
2023-02-13 08:32:16 +0100

    Move missing tests to jose

--- test/tests.ml
+++ test/tests.ml
@@ -2,7 +2,7 @@ open OUnit2
 
 let () =
   let tests =
-    B64u_test.all_tests @ Jwk_test.all_tests @ Jws_test.all_tests
+    B64u_test.all_tests
   in
   let suite = "suite">::: tests in
   Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna);
