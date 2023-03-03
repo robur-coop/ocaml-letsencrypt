a8f53fa30f654363042344e95324e5cdcb037eb8
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-03-03 10:20:02 +0100

    Be able to specify protocols available for the ALPN server

--- mirage/lE_http_server.mli
+++ mirage/lE_http_server.mli
@@ -39,12 +39,17 @@ module Make
     Http_mirage_client.t ->
     (Paf.TLS.flow, Ipaddr.t * int) Alpn.server_handler ->
     (unit, [> `Msg of string ]) result Lwt.t
-  (** [with_lets_encrypt_certificates ?port stackv4v6 ~production cfg http
-      handler] launches 2 servers:
-      1) An HTTP server which handles let's encrypt challenges and redirections
-      2) An ALPN server (HTTP/1.1 and H2) servers to the user's request handler
-
+  (** [with_lets_encrypt_certificates ?port ?alpn_protocols stackv4v6 ~production cfg handler]
+      launches 2 servers:
+      - An HTTP server which handles let's encrypt challenges and redirections
+      - An ALPN server (which handles HTTP/1.1 and H2 by default, otherwise you
+        can specify protocols via the [alpn_protocol] argument) which run the
+        user's request handler
+  
       Every 80 days, the fiber re-askes a new certificate from let's encrypt and
       re-update the ALPN server with this new certificate. The HTTP server does
-      the redirection to the hostname defined into the given [cfg]. *)
+      the redirection to the hostname defined into the given [cfg].
+  
+      {b NOTE}: For the [alpn_protocols] argument, only ["h2"], ["http/1.1"] and
+      ["http/1.0"] are handled. Any others protocols will be {b ignored}! *)
 end
